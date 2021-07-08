1643cbd6363ffdb3e5a61e1b1c76a8bc78226ff4
Hamza Khan <hamzakhan@Mac-HamzaKhan-py-452.local>
2021-07-08 18:31:49 +0500

    after airbnb style guide

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => (
   <div className="wrapper">
     <div>
       <p>
-        <strong>Dennis Gecaj</strong> is a <span className="yellowText">full stack product designer</span> from Germany working in Tech with a <span className="yellowText">master of science degree</span> in industrial design from <span className="yellowText">TUDelft</span>. He has first hand industry experince and recently founded his own startup <span className="yellowText">Yello.</span>
+        <strong>Dennis Gecaj</strong> is a <span className="yellowText">full stack product designer</span> from Germany working in Tech with a <span className="yellowText">master of science degree</span> in industrial design from <span className="yellowText">TUDelft</span>. He has first hand industry experince and recently founded his own startup <span className="yellowText">Yello..</span>
       </p>
     </div>
     <button>About</button>
