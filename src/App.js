6c61d621913f9f2b81db32a9ed98ab7d2f0c7c90
Hamza Khan <hamzakhan@Mac-HamzaKhan-py-452.local>
2021-07-08 18:29:28 +0500

    after airbnb style guide

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from "react";
-import './css/app.css'
-const App = () => {
-    return (
-        <div className="wrapper">
-            <div>
-                <p><strong>Dennis Gecaj</strong> is a <span className="yellowText">full stack product designer</span> from Germany working in Tech with a <span className="yellowText">master of science degree</span> in industrial design from <span className="yellowText">TUDelft</span>. He has first hand industry experince and recently founded his own startup <span className="yellowText">Yello.</span></p>
-            </div>
-            <button>About</button>
-        </div>
-    )
-};
+import React from 'react';
+import './css/app.css';
+
+const App = () => (
+  <div className="wrapper">
+    <div>
+      <p>
+        <strong>Dennis Gecaj</strong> is a <span className="yellowText">full stack product designer</span> from Germany working in Tech with a <span className="yellowText">master of science degree</span> in industrial design from <span className="yellowText">TUDelft</span>. He has first hand industry experince and recently founded his own startup <span className="yellowText">Yello.</span>
+      </p>
+    </div>
+    <button>About</button>
+  </div>
+);
 
 export default App;
